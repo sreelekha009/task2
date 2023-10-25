d1d802df3a1bd77665acd6a86a9beb0dc5490d2f
sreelekha009 <sreelekhaediga09@gmail.com>
2023-10-25 23:11:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Caluclator from './Caluclator';
+
+function App() {
+  return (
+    <div className="App">
+      <Caluclator/>
+    </div>
+  );
+}
+
+export default App;
